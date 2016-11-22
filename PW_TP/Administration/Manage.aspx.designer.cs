6e4e42a96902df310c48e6287612a4423c0378f4
f4biocoito <f4bio.coito@outlook.pt>
2016-11-22 01:28:59 +0000

    administator manage
    alteracoes no design e melhoramento de alguns aspetos

--- PW_TP/Administration/Manage.aspx.designer.cs
+++ PW_TP/Administration/Manage.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace PW_TP.Administration {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// BtEditar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtEditar;
     }
 }
